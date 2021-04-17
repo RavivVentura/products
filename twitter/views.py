935ea8970575b4f31a6326ba24db34f19319d0f4
stav <stav@everthere.co>
2021-04-17 17:24:21 +0300

    add params to urls & add a form

--- twitter/views.py
+++ twitter/views.py
@@ -10,9 +10,12 @@ def index(request):
     context = {
         'latest_twitter_handle_list': latest_twitter_handle_list,
     }
-    # if request.method == "POST":
-    #     handle = request.POST.get('twitter_handle')
-    #     return render(request, 'twitter/detail.html', {'handle': handle})
+    if request.method == "POST":
+        print("post")
+        twitter_handle = request.POST.get('twitter_handle')
+        folder_id = request.POST.get('folder_id')
+        load_company_tweets_into_csv_file(twitter_handle, folder_id)
+        return render(request, 'twitter/detail.html', {'handle': twitter_handle})
     return render(request, 'twitter/index.html', context)
 
 def get_twitter_handle(request, twitter_handle , folder_id):
@@ -20,13 +23,10 @@ def get_twitter_handle(request, twitter_handle , folder_id):
     load_company_tweets_into_csv_file(twitter_handle,folder_id)
     return render(request, 'twitter/detail.html', {'handle':twitter_handle})
 
-# def results(request, twitter_handle):
-#     response = "You're looking at the results of twitter_handle %s."
-#     return HttpResponse(response % twitter_handle)
-#
-# def get_webinars(request ,twitter_handle):
-#     response = "You're looking at the get_webinars of twitter_handle %s."
-#     return HttpResponse(response % twitter_handle)
-
+def getwebinars(request):
+    twitter_handle = request.GET.get('twitter_handle', '')
+    folder_id = request.GET.get('folder_id', '')
+    load_company_tweets_into_csv_file(twitter_handle, folder_id)
+    return render(request, 'twitter/detail.html', {'handle':twitter_handle})
 
 
