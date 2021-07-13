c6811ab264f74e152a47274b116714582cdb5d48
stav <stav@everthere.co>
2021-07-13 19:36:56 +0300

    remove environ

--- twittersite/settings.py
+++ twittersite/settings.py
@@ -16,8 +16,6 @@ import dj_database_url
 from django.core.management.utils import get_random_secret_key
 
 # Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
