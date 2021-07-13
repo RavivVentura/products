58e863c505aab0ea7a23f7095014ec5e93ed41af
stav <stav@everthere.co>
2021-07-13 19:30:05 +0300

    remove import environ

--- twittersite/settings.py
+++ twittersite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 import sys
-import environ
 from pathlib import Path
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
